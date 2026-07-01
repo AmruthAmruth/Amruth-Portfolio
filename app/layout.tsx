3bb5049d6b925590c4a63cf5f22450766834011a
Amruth Shyju <amruthshyju@gmail.com>
2026-07-01 18:35:35 +0530

    fix : fix the issue in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,7 @@ export default function RootLayout({
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <Navbar />
         <CustomCursor />
